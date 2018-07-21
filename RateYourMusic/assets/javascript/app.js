b4c55292319ffc919142a75a2cab9d59f5e8bb83
burritoscott <36803614+burritoscott@users.noreply.github.com>
2018-07-21 10:16:27 -0700

    ajax is messed up

--- RateYourMusic/assets/javascript/app.js
+++ RateYourMusic/assets/javascript/app.js
@@ -1,22 +1,23 @@
-  $(document).ready(function(){
-    $('#artistRateDiv').hide();
-    //controls the carousel
-    $('.carousel').carousel({
-      interval: 2500
-    })
-  });
-  $('button').on('click', function(){
-    event.preventDefault();
-    $('#artistRateDiv').show();
+$(document).ready(function () {
+  $('#artistRateDiv').hide();
+  ///////////////////////controls the carousel
+  $('.carousel').carousel({
+    interval: 2500
+  })
+  ///////////////////////
+});
+$('button').on('click', function () {
+  event.preventDefault();
+  $('#artistRateDiv').show();
 
-    var artist = $('#inputBand').val();
+  var artist = $('#inputBand').val();
 
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist +
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist +
     "?app_id=codingbootcamp";
 
-$.ajax({
-  url: queryURL,
-}).then(function(response) {
+  $.ajax({
+    url: queryURL,
+  }).then(function (response) {
     console.log(response);
 
     var name = response.name;
@@ -24,40 +25,40 @@ $.ajax({
     var poster = response.image_url;
     var image = $('#image').attr({
       'src': poster,
-      'width' : '200',
-      'height' : '200'
+      'width': '200',
+      'height': '200'
     });
 
     $('#bandInfo').append(name);
     $('#artistRateDiv').append(image);
 
-    });
   });
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA2vvBDumPn-X8UqCtoyVQASetXVlmRt04",
-    authDomain: "bandsrating.firebaseapp.com",
-    databaseURL: "https://bandsrating.firebaseio.com",
-    projectId: "bandsrating",
-    storageBucket: "bandsrating.appspot.com",
-    messagingSenderId: "245069074646"
-  };
-  firebase.initializeApp(config);
-
-  // <-- Create a variable to reference the database -->
-
-  var database = firebase.database();
-
-  $('button').on('click', function(){
-      event.preventDefault();
-
-      database.ref().set({
-rating: $('select').val()
-});
 });
 
-database.ref().on("value", function(snapshot) {
-$('span').text(snapshot.val().rating);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA2vvBDumPn-X8UqCtoyVQASetXVlmRt04",
+  authDomain: "bandsrating.firebaseapp.com",
+  databaseURL: "https://bandsrating.firebaseio.com",
+  projectId: "bandsrating",
+  storageBucket: "bandsrating.appspot.com",
+  messagingSenderId: "245069074646"
+};
+firebase.initializeApp(config);
 
+// <-- Create a variable to reference the database -->
+
+var database = firebase.database();
+
+$('button').on('click', function () {
+  event.preventDefault();
+
+  database.ref().set({
+    rating: $('select').val()
   });
+});
+
+database.ref().on("value", function (snapshot) {
+  $('span').text(snapshot.val().rating);
+
+});
